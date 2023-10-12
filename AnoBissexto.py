d509fff986b676078be686aac187bbdde3943343
Francisco Ferreira de Araujo <fferreira.araujo@hotmail.com>
2023-10-12 06:30:19 -0300

    Reta Final

--- AnoBissexto.py
+++ AnoBissexto.py
@@ -1,9 +1,9 @@
+from datetime import date
 ano = int (input(' Digite um ano qualquer: '))
-print(30*'+')
-print (ano)
-print(30*'+')
-
+if ano == 0:
+    ano = date.today().year
+    print (ano)
 if (ano%4)==0 and (ano%100)!=0 or (ano%400)==0:
-    print ('É ano Bissexto!')
+    print ('É ano Bissexto!'.format(ano))
 else:
-    print (' Não é ano Bissexto!')
+    print (' Não é ano Bissexto!'.format(ano))
